663e191065ca331a35552988a774b866f841e259
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2022-12-10 10:24:53 +0530

    generic type implementation

---
+++ generic-type.ts
@@ -0,0 +1,25 @@
+const phoneList = [
+  { customerId: "0001", areaCode: "321", num: "123-4566" },
+  { customerId: "0002", areaCode: "174", num: "142-3626" },
+  { customerId: "0003", areaCode: "192", num: "012-7190" },
+  { customerId: "0005", areaCode: "402", num: "652-5782" },
+  { customerId: "0004", areaCode: "301", num: "184-8501" },
+]
+
+interface PhoneInfo {
+  customerId: string
+  areaCode: string
+  num: string
+}
+ 
+function listToDict<T>(lists: T[], callback: (list: T) => string): { [k:string]: T} {
+  const dicWithList: { [k:string]: T} = {};
+
+  lists.forEach(list => {
+    const dictKey = callback(list);
+    dicWithList[dictKey] = list;
+  })
+  return dicWithList;
+}
+
+listToDict<PhoneInfo>(phoneList, (list) => list.customerId);
